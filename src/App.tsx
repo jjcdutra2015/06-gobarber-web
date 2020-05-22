b557ee40932a7847dd0e64f1296c30fb67dde914
Julio Dutra <jjcdutra@gmail.com>
2020-05-22 20:30:12 -0300

    Estrutura de toast

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyles from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
-      <GlobalStyles />
     </AuthProvider>
+
+    <ToastContainer />
+
+    <GlobalStyles />
   </>
 );
 
